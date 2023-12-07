abeead04de112694ba2d3e3d79818989d0deeb84
boshra khalid <boshrakhalid6@gmail.com>
2023-12-07 09:27:11 +0300

    main page front end completed

---
+++ lib/ascii_conversion.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class ascii_conversion extends StatelessWidget {
+  const ascii_conversion({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("ascii conversion page");
+  }
+}
---
+++ lib/binary_conversion.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class Binary_Conversion extends StatelessWidget {
+  const Binary_Conversion({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("binary conversion page");
+  }
+}
