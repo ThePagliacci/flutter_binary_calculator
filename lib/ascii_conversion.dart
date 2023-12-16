58d152af82e00c1f117fe1a8c49d76fa99cc4625
boshra khalid <boshrakhalid6@gmail.com>
2023-12-16 21:10:32 +0300

    ascii conversion page frontend section completed

--- lib/ascii_conversion.dart
+++ lib/ascii_conversion.dart
@@ -10,6 +10,9 @@ class Ascii_Conversion extends StatefulWidget {
 
 class _Ascii_ConversionState extends State<Ascii_Conversion> {
   var binaryButtonlar = ["0", "1", "CE", "DEL", " "];
+
+  var asciiButtonlar =  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+                                    "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "CE", "DEL", " "];
   String system ="";
   String sonuc = "";
   int ptr = 2;
@@ -85,7 +88,7 @@ class _Ascii_ConversionState extends State<Ascii_Conversion> {
               ),
             ),
             Container(
-              height: 200.0,
+              height: 100.0,
               width: 900.0,
               margin: EdgeInsets.all(8.0),
               padding: EdgeInsets.all(8.0),
@@ -93,7 +96,7 @@ class _Ascii_ConversionState extends State<Ascii_Conversion> {
                 physics: NeverScrollableScrollPhysics(), // Disable scrolling
                 itemCount: binaryButtonlar.length,
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 5, crossAxisSpacing: 20, mainAxisSpacing: 20,childAspectRatio: 2.0,),
+                    crossAxisCount: 5, crossAxisSpacing: 20, mainAxisSpacing: 20,childAspectRatio: 3.0,),
                 itemBuilder: (context, index) {
                   return InkWell(
                     splashColor: Colors.black,
@@ -129,7 +132,6 @@ class _Ascii_ConversionState extends State<Ascii_Conversion> {
                 },
               ) ,
             ),
-
             Padding(
               padding: EdgeInsets.all(16.0),
               child: Row(
@@ -153,7 +155,6 @@ class _Ascii_ConversionState extends State<Ascii_Conversion> {
                 ],
               ),
             ),
-
             Padding(
               padding: EdgeInsets.all(16.0),
               child: Row(
@@ -178,7 +179,51 @@ class _Ascii_ConversionState extends State<Ascii_Conversion> {
                 ],
               ),
             ),
-
+            Container(
+              height: 400.0,
+              width: 1000.0,
+              margin: EdgeInsets.all(8.0),
+              padding: EdgeInsets.all(8.0),
+              child: GridView.builder(
+                physics: NeverScrollableScrollPhysics(), // Disable scrolling
+                itemCount: asciiButtonlar.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 7, crossAxisSpacing: 20, mainAxisSpacing: 20,childAspectRatio: 3),
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    splashColor: Colors.black,
+                    onTap: () {
+                      setState(() {
+                        //convertBinaryFraction(binaryButtonlar[index], system);
+                      });
+                    },
+                    child: Container(
+                      color: Colors.grey.shade700,
+                      alignment: Alignment.center,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Text(
+                            asciiButtonlar[index],
+                            style: TextStyle(
+                              fontSize: 29.0,
+                              fontFamily: "Silkscreen",
+                              color: Colors.white,
+                            ),
+                          ),
+                          if (index == 28) // Conditionally add icon for the first grid item
+                            Icon(
+                              Icons.swap_vert_circle, // Replace with the desired icon
+                              size: 40.0,
+                              color: Colors.white,
+                            ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ) ,
             Container(
               margin: EdgeInsets.all(50.0),
               child:
